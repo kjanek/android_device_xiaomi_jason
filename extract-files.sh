80148133512fc70e8648fbfd7fa840a454d3a51b
Jyotiraditya Panda <jyotiraditya@aospa.co>
2022-04-25 20:56:35 -0400

    sm8350-common: Do not configure RPC threadpool in camera HAL
    +turn camera postproc's and camera.xiaomi.so thread poo
     * Hex edit the blob to remove calls to configureRpcThreadpool
       function, by executing NOP
    
     * These calls kill camera with
       Abort message: 'Binder threadpool cannot be shrunk after starting'
    
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: If81c3e67a695faae9e14affa66319cb63da4399d
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -21,6 +21,19 @@ function blob_fixup() {
         vendor/lib/libmpbase.so)
             "${PATCHELF}" --remove-needed "libandroid.so" "${2}"
             ;;
+	vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            hexdump -ve '1/1 "%.2X"' "${2}" | sed "s/8D0A0094AE1640F9/1F2003D5AE1640F9/g" | xxd -r -p > "${TMPDIR}/${1##*/}"
+            mv "${TMPDIR}/${1##*/}" "${2}"
+            ;;
+        vendor/lib64/hw/camera.xiaomi.so)
+            # Before
+            # 21 00 80 52     mov        w1,#0x1
+            # 29 07 00 94     bl         <EXTERNAL>::android::hardware::configureRpcThr
+            # After
+            # 21 00 80 52     mov        w1,#0x1
+            # 1f 20 03 d5     nop
+            sed -i "s/\x21\x00\x80\x52\x29\x07\x00\x94/\x21\x00\x80\x52\x1f\x20\x03\xd5/g" "${2}"
+            ;;
     esac
 }
 
